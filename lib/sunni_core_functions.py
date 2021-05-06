0f69e508c53d508bccdfa3e4378abdca7c1deaa8
Andrew Dean <oneandydean@hotmail.com>
2021-05-06 22:51:26 +0100

    Remove some code that's now been reimplemented

--- lib/sunni_core_functions.py
+++ lib/sunni_core_functions.py
@@ -1,7 +1,3 @@
-import pygame
-
-
-# Defining a function to delete save files
 def deletesave(savefile):
     line_number = 0
     save = open(file_directory + "Save Files\save" + savefile + ".txt", "r")
@@ -18,15 +14,3 @@ def deletesave(savefile):
     for line in range(number_of_lines - 1):
         save.write("\n")
     save.close()
-
-
-# Definining a function to make the screen fade out or in
-def fade(direction,opacity):
-    fade_overlay = pygame.image.load(file_directory + "images/sunni_fade_overlay" + str(opacity) + ".png").convert_alpha()
-    screen.blit(fade_overlay, (0,0))
-    
-    if direction == "in":
-        new_opacity = opacity - 10
-    elif direction == "out":
-        new_opacity = opacity + 10
-    return new_opacity
