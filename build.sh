e716ba397e625d03589f58c31234bfdd758c56dd
Andrew Dean <oneandydean@hotmail.com>
2021-05-07 18:25:08 +0100

    Update build.sh to create a directory containing everything you need to distribute and cleans up the mess building makes. Distributable needs zipping still.

--- build.sh
+++ build.sh
@@ -1,2 +1,21 @@
 #!/bin/bash
 pyinstaller --onefile main.py --noconsole
+
+mkdir sunni
+cp -r audio/ sunni
+cp -r images/ sunni
+cp -r lib/ sunni
+cp -r dist/ sunni
+
+mkdir sunni/saves
+echo 'No save data' > sunni/saves/save1.txt
+echo 'No save data' > sunni/saves/save2.txt
+echo 'No save data' > sunni/saves/save3.txt
+echo 'No save data' > sunni/saves/save4.txt
+
+cp 'Play Sunni.lnk' sunni
+
+echo Please zip up the sunni/ folder. This sunni.zip is your distributable.
+
+rm -rf build/
+rm -rf dist/
